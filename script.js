a402a344605e7bc959097dfdaff01a9ff8ef6229
campus001 <campusnuevo2019ucentral@gmail.com>
2024-07-31 23:36:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
 
-function nextSlide() {
-  slides[currentSlide].style.marginLeft = '-100%';
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].style.marginLeft = '0';
-}
+document.addEventListener("DOMContentLoaded", ()=>{
+    const elementosCarousel = document.querySelectorAll(".carousel");
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
+        indicators: true,
+        noWrap: false
 
-// Llama a nextSlide() para avanzar a la siguiente diapositiva
+    });
+});
+// const slides = document.querySelectorAll('.slide');
+//let currentSlide = 0;
+
+//function nextSlide() {
+ // slides[currentSlide].style.marginLeft = '-100%';
+  //currentSlide = (currentSlide + 1) % slides.length;
+ // slides[currentSlide].style.marginLeft = '0';
+//}Llama a nextSlide() para avanzar a la siguiente diapositiva
