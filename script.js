8a62ebc8473019c5d9d2620ce273bb562aa621ac
campus001 <campusnuevo2019ucentral@gmail.com>
2024-07-31 21:31:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].style.marginLeft = '-100%';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.marginLeft = '0';
+}
+
+// Llama a nextSlide() para avanzar a la siguiente diapositiva
